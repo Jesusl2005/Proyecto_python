cf08d38242fdd6b6de9449fcb6adb5eaf35b7b02
Jesus Lopez <lopezramosjesusdavid@gmail.com>
2023-08-17 16:11:21 -0500

    Arreglando detalles adicionales en el modulo elimar

--- Eliminar.py
+++ Eliminar.py
@@ -1,6 +1,9 @@
 import PySimpleGUI as sg
 import pandas as pd
 
+import PySimpleGUI as sg
+import pandas as pd
+
 def eliminar(dicts: pd.DataFrame):
     sg.theme('LightBlue')
     layout = [
@@ -11,30 +14,38 @@ def eliminar(dicts: pd.DataFrame):
     window1 = sg.Window('Eliminar registro', layout)
     while True:
         event, values = window1.read()
+        if event == sg.WIN_CLOSED or event == 'Salir':
+            break
         if event == 'Eliminar':
             try:
                 valor = values['valor']
                 valor = int(valor) - 1
-                valores = dicts[valor]
-                df_valores = pd.DataFrame(valores)
-                layout2 = [
-                    [sg.Table(values=df_valores.values.tolist(), headings=list(dicts.keys()), display_row_numbers=False,
-                            auto_size_columns=True, num_rows=min(25, len(dicts)), justification='center',
-                            key='-TABLE-')],
-                    [sg.Text('Desea eliminar el registro?', expand_x=True, justification='center')],
-                    [sg.Button('Aceptar', expand_x=True), sg.Button('Cancelar', expand_x=True)]
-                ]
-                window2 = sg.Window('Confirmacion', layout2)
-                while True:
-                    event, values = window2.read()
-                    if event == 'Aceptar':
-                        dict.drop(values['valor'])
-                        sg.popup('El registro se ha eliminado correctamente', title='Proceso exitoso')
-                    if event == 'Cancelar' or event == sg.WIN_CLOSED:
-                        break
-                window2.close()
+                
+                if valor >= 0 and valor < len(dicts):  # Check if the index is within bounds
+                    df_valores = dicts.iloc[valor:valor+1]  # Get a DataFrame row by index
+                    layout2 = [
+                        [sg.Table(values=df_valores.values.tolist(), headings=list(dicts.columns), display_row_numbers=False,
+                                  auto_size_columns=True, num_rows=min(25, len(df_valores)), justification='center',
+                                  key='-TABLE-')],
+                        [sg.Text('Desea eliminar el registro?', expand_x=True, justification='center')],
+                        [sg.Button('Aceptar', expand_x=True), sg.Button('Cancelar', expand_x=True)]
+                    ]
+                    window2 = sg.Window('Confirmacion', layout2)
+                    while True:
+                        event2, values2 = window2.read()
+                        if event2 == sg.WIN_CLOSED or event2 == 'Cancelar':
+                            break
+                        if event2 == 'Aceptar':
+                            dicts.drop(index=valor, inplace=True)  # Remove row from the DataFrame
+                            sg.popup('El registro se ha eliminado correctamente', title='Proceso exitoso')
+                            window1['valor'].update('')
+                            dicts.set_index('id', inplace=True)
+                            dicts.to_csv('registros_gastos.csv', index=True)
+                            break
+                    window2.close()
+                else:
+                    sg.popup_error('¡Error! El valor ingresado no es un índice válido.')
             except ValueError:
                 sg.popup_error('¡Error! Ingrese un número entero válido.')
-        if event == 'Salir' or event == sg.WIN_CLOSED:
-            break
+    
     window1.close()
--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import os
 
 def main():
-
     border_style = {
         "border_width": 2,
         "relief": tk.SOLID
